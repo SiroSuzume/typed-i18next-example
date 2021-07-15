2f5234669189be625602294ecb7fc61259c766ea
sirosuzume <nicorette.kinen@gmail.com>
2021-07-15 11:43:52 +0900

    :globe_with_meridians: 馬名

---
+++ src/locales/en/horse-names/index.ts
@@ -0,0 +1,15 @@
+import type { HorseNamesTranslationResource } from '@/locales/ja/horse-names';
+
+/** 馬名翻訳リソース en */
+export const enHorseNamesTranslation: HorseNamesTranslationResource = {
+  ゴールドシップ: 'Gold Ship',
+  ステイゴールド: 'Stay Gold',
+  オルフェーヴル: 'Orfevre',
+  カワカミプリンセス: 'Kawakami Princess',
+  ナカヤマフェスタ: 'Nakayama Festa',
+  スイープトウショウ: 'Sweep Toushou',
+  エアシャカール: 'Air Shakur',
+  ドリームジャーニー: 'Dream Journey',
+} as const;
+
+export default enHorseNamesTranslation;

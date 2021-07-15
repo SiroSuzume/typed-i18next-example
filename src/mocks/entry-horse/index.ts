151a7ac40e17d0c8aab159e12feb502b3b0cd6fc
sirosuzume <nicorette.kinen@gmail.com>
2021-07-15 15:50:09 +0900

    :clown_face: mockで出走馬情報

---
+++ src/mocks/entry-horse/index.ts
@@ -0,0 +1,12 @@
+import { EntryHorse } from '@/types/entry-horses';
+
+export const mockEntryHorses: EntryHorse[] = [
+  { id: 2009102739, horseName: 'ゴールドシップ', horseNumber: 1 },
+  { id: 1997103398, horseName: 'エアシャカール', horseNumber: 2 },
+  { id: 2004102753, horseName: 'ドリームジャーニー', horseNumber: 3 },
+  { id: 2008102636, horseName: 'オルフェーヴル', horseNumber: 4 },
+  { id: 2001104313, horseName: 'スイープトウショウ', horseNumber: 5 },
+  { id: 2003107045, horseName: 'カワカミプリンセス', horseNumber: 6 },
+  { id: 2006102424, horseName: 'ナカヤマフェスタ', horseNumber: 7 },
+  { id: 1994108729, horseName: 'ステイゴールド', horseNumber: 8 },
+];

5093db9f63c12ddd113f3accd309c3c0b0e2251b
sirosuzume <nicorette.kinen@gmail.com>
2021-07-17 23:39:27 +0900

    :recycle: next-18nを使用するように修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,28 @@ import { MyDefaultSeo } from '@/components/organisms/seo/default';
 import { TopPageSeo } from '@/components/organisms/seo/top';
 import { TopTemplate } from '@/components/templates/top';
 import { NameSpace } from '@/lib/i18next/types';
-import { importI18nextResource, ImportedResource, addImportedResources } from '@/lib/i18next/loader';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { EntryHorse } from '@/types/entry-horses';
 import { mockEntryHorses } from '@/mocks/entry-horse';
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
+import nextI18NextConfig from '@/../next-i18next.config.js';
 
 /** トップページで使用するネームスペース一覧 */
 const i18nextNameSpaces: NameSpace[] = ['seo', 'common', 'horse-names', 'top'];
 
 type TopPageProps = {
-  resources: ImportedResource[];
   entryHorses: EntryHorse[];
 };
 
-export const getStaticProps: GetStaticProps<TopPageProps> = async ({ locale, defaultLocale }) => {
+export const getStaticProps: GetStaticProps<TopPageProps> = async ({ locale }) => {
   // 必要なリソースをロードする
-  const resources = await importI18nextResource([locale!], defaultLocale!, i18nextNameSpaces);
-  return { props: { resources, entryHorses: mockEntryHorses } };
+  const translation = await serverSideTranslations(locale!, i18nextNameSpaces, nextI18NextConfig);
+  return { props: { ...translation, entryHorses: mockEntryHorses } };
 };
 
 /** トップページ */
-const TopPage: NextPage<TopPageProps> = ({ resources, entryHorses }) => {
-  addImportedResources(resources);
+const TopPage: NextPage<TopPageProps> = ({ entryHorses }) => {
   const { locale } = useRouter();
   const myDefaultSeo = useMemo(() => <MyDefaultSeo />, [locale]);
   const topPageSeo = useMemo(() => <TopPageSeo />, [locale]);

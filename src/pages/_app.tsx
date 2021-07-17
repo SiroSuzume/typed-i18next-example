c4ecff3ff8b6b07f84f9af9b46168f6a96273abf
sirosuzume <nicorette.kinen@gmail.com>
2021-07-17 14:41:41 +0900

    :bug: i18nのinitializeが複数走ってしまう構造の修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { customTheme } from '@/theme';
 import { AppLayout } from '@/components/templates/app';
+import { defaultLocale, supportedLocales } from '@/lib/env';
+import { nameSpaces } from '@/lib/i18next/types';
+import { initializeI18n } from '@/lib/i18next/initializer';
+
+initializeI18n(supportedLocales, [...nameSpaces], defaultLocale);
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   <ChakraProvider resetCSS theme={customTheme}>

d0e61125d6072fe1fc648fddbb30c7561648929f
sirosuzume <nicorette.kinen@gmail.com>
2021-07-18 14:31:56 +0900

    :wrench: storybookのi18n対応

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,40 @@
-import { addDecorator } from '@storybook/react';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import React, { Suspense } from 'react';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { i18n as i18nextConfig } from '../next-i18next.config';
+import { withI18next } from 'storybook-addon-i18next';
 
-addDecorator((storyFn) => <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>);
+i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    whitelist: i18nextConfig.locales,
+    lng: i18nextConfig.defaultLocale,
+    supportedLngs: i18nextConfig.locales,
+    fallbackLng: i18nextConfig.defaultLocale,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export const decorators = [
+  withI18next({
+    i18n: i18next,
+    languages: {
+      ja: '日本語',
+      en: 'English',
+      zh: '中文',
+    },
+  }),
+  (Story) => (
+    <Suspense fallback="loading...">
+      <Story />
+    </Suspense>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
